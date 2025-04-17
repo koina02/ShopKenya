dd6516d493becc85a296e2174d75a7c00449107b
Koina Ian <koinaobedian@gmail.com>
2025-04-17 11:12:28 +0300

    Updated pages to use consistent navbar from navbar.html

--- layout.js
+++ layout.js
@@ -1,3 +1,9 @@
-// layout.js
-fetch("navbar.html").then(res => res.text()).then(data => document.getElementById("navbar").innerHTML = data);
-fetch("footer.html").then(res => res.text()).then(data => document.getElementById("footer").innerHTML = data);
+// Inject navbar content into the element with id "navbar"
+fetch("navbar.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("navbar").innerHTML = data);
+
+// Inject footer content into the element with id "footer"
+fetch("footer.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("footer").innerHTML = data);
